689a0e16ad6e3a0e637174c579d91ac6cedc24c2
Amalie <amaliegay@outlook.com>
2023-05-26 06:09:42 -0400

    v0.0.1

---
+++ dataset.py
@@ -0,0 +1,50 @@
+import os
+from PIL import Image
+import random
+
+class DataLoader:
+    def __init__(
+        self,
+        n_sample: int = 10,
+        random_state: int = 0,
+        file_path: str = f"{os.path.dirname(os.path.realpath(__file__))}/data",
+    ) -> None:
+        super().__init__()
+        self.n_sample = n_sample
+        self.random_state = random_state
+        self.file_path = file_path
+        
+        print(f"Load data from: {self.file_path}")
+
+    def setup(self) -> None:
+        with open(f"{self.file_path}/labels.txt", "r", encoding="utf-8") as labels:
+            lines: list[str] = labels.readlines()
+            data = {"img_names":[],"imgs":[],"expressions":[]}
+
+            random.seed(self.random_state)
+            lines = random.choices(lines,k=self.n_samples)
+            
+            for line in lines:
+                line_split = line.strip().split()
+                img_name = line_split[0]
+                expression = line_split[1:]
+                with open(f"{self.file_path}/features/{img_name}.bmp", "r") as f:
+                    # move image to memory immediately, avoid lazy loading, 
+                    # which will lead to None pointer error in loading
+                    img = Image.open(f).copy()
+                # Insert a tuple consist of the file name of the image, the image, 
+                # and the LaTeX expression in the image
+                data["img_names"].append(img_name)
+                data["imgs"].append(img)
+                data["expressions"].append(expression)
+
+        self.dataset = data
+
+def build_dataset(n_sample: int,random_state: int=0):
+    dl = DataLoader(n_sample, random_state)
+    dl.setup()
+    data = dl.dataset
+    return dl.dataset.img_names, data.imgs, data.expressions
+
+if __name__ == "__main__":
+    dataset = build_dataset
---
+++ main.py
@@ -0,0 +1,56 @@
+import argparse
+from PIL import Image
+import torch
+from torch import nn
+from torch import FloatTensor, LongTensor
+import torchvision
+
+from dataset import dataset
+from decoder import Decoder
+from encoder import Encoder
+
+# Load number of samples
+n_samples = 10
+
+# Manual seed
+random_state = 258
+
+# Table of image name, image, and the expression in the image
+file_names, features, labels = dataset(n_samples,random_state=random_state)
+
+# Turn image data into tensors
+for feature in features:
+    feature = torchvision.transforms.ToTensor()(feature)
+
+# Split data into training and test sets
+# 20% of data will be test and 80% will be train
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels,test_size=0.2,random_state=random_state)
+
+# Building a model
+
+# Device-agnostic code
+parser = argparse.ArgumentParser(description="Device-agnostic code")
+parser.add_argument('--disable-cuda', action='store_true',help="Dsiable CUDA")
+args = parser.parse_args()
+args.device = None
+if not args.disable_cuda and torch.cuda.is_available():
+    args.device = torch.device("cuda")
+    print("")
+else:
+    args.device = torch.device("cpu")
+
+# Construct a model that subclasses nn.Module
+class BTTR(nn.Module):
+    def __init__(self,
+                 # Encoder
+                 growth_rate:int):
+        super().__init__()
+        # Create nn layers
+        self.encoder = Encoder(growth_rate=growth_rate)
+        self.decoder = Decoder()
+
+    def forward(self, img: FloatTensor, tgt: LongTensor) -> FloatTensor:
+        feature = self.encoder(img)
+        out = self.decoder(feature, tgt)
